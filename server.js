c1da78580f73b12684eb66c1099c2c7073ab246b
Abiodun001-world <adekunleabiodun354@gmail.com>
2023-09-06 21:14:37 +0100

    fixed an error

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import routesController from './routes/index';
+import controllerRoutes from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-routesController(app);
+controllerRoutes(app);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
